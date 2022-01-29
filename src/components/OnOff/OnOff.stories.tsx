fac04336027e77ff68317ac2c186550f3c606843
sveta-confeta <_svet_@tut.by>
2022-01-29 16:22:39 +0300

    OnOff

--- src/components/OnOff/OnOff.stories.tsx
+++ src/components/OnOff/OnOff.stories.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-// import { ComponentStory, ComponentMeta } from '@storybook/react';
+ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import {OnOff,} from './OnOff'
 
 export default {
@@ -7,20 +7,11 @@ export default {
     component: OnOff,
 };
 
-export const EmptyRaiting = () => <Raiting value={0} onClick={x=>x} />
+export const onMode = () => <OnOff on={true}  onClick={x => x} />
+export const offMode = () => <OnOff on={false}  onClick={x => x} />
 
-export const Raiting1 = () => <Raiting value={1} onClick={x=>x} />
-
-export const Raiting2 = () => <Raiting value={2} onClick={x=>x} />
-
-export const Raiting3 = () => <Raiting value={3} onClick={x=>x} />
-
-export const Raiting4 = () => <Raiting value={4} onClick={x=>x}/>
-
-export const Raiting5 = () => <Raiting value={5} onClick={x=>x}/>
-
-export const RaitigChange = () => {
-    const [raiting, setRaiting] = useState<RaitingValueType>(5);
-    return <Raiting value={raiting} onClick={setRaiting}/>
+export const ModeChanging = () => {
+    const [value, setValue] = useState<boolean>(true);
+    return <OnOff on={value} onClick={setValue}/>
 };
 
